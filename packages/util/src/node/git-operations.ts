1c3f182b6c73b19c46db073594b6cb3c9957c801
golive-dev <dev@golive.me>
2025-10-25 10:37:31 -0500

    fix: remove unused @ts-expect-error directive

--- packages/util/src/node/git-operations.ts
+++ packages/util/src/node/git-operations.ts
@@ -158,7 +158,6 @@ export class GitOperations {
 
   private async initializeSimpleGit(): Promise<void> {
     try {
-      // @ts-expect-error - Dynamic import of optional dependency
       const simpleGitModule = await import('simple-git')
       this.git = simpleGitModule.simpleGit(this.workingDir)
     }
